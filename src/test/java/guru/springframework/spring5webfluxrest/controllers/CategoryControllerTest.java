a1e4d2afc1d51d956f0d530c46b84857152bf664
Ferenc Bondar <fhery021@gmail.com>
2019-06-25 14:29:19 +0300

    vendor controller and test

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.notNull;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(
+                        Category.builder().description("hello").build(),
+                        Category.builder().description("desc 2").build()));
+        webTestClient.get().uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(categoryRepository.findById(any(String.class)))
+                .willReturn(Mono.just(
+                        Category.builder().description("a category").build()
+                ));
+        webTestClient.get().uri("/api/v1/categories/1")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
